043855eab522111cbe20bc3edd1c4709e51595fa
zafeirian <zafosnikos@gmail.com>
2025-08-31 18:15:13 +0300

    Added .env file and create_vdb() function

--- main.py
+++ main.py
@@ -4,13 +4,20 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 import re
 from langchain_core.documents import Document
+from langchain_chroma import Chroma
+from langchain_openai.embeddings import OpenAIEmbeddings
+import shutil
 
 DATA_PATH = "data/books/"
+CHROMA_PATH = "chroma"
+load_dotenv()
+embedding_function = OpenAIEmbeddings(api_key=os.getenv("OPENAI_API_KEY"))
+
+
 
 def load_documents():
     loader = PyPDFLoader(DATA_PATH + "the-hobbit.pdf")
     documents = loader.load()
-    print(len(documents))
     for doc in documents:
         doc.page_content = re.sub(r"\s+", " ", doc.page_content).strip()
     return documents
@@ -25,8 +32,21 @@ def split_into_chunks(documents: list[Document]):
     chunks = text_splitter.split_documents(documents)
     return chunks
 
-documents = load_documents()
-chunks = split_into_chunks(documents)
+def create_vdb(chunks: list[Document], embedding_function = embedding_function):
+    # Deleting previous VDBases
+    if os.path.exists(CHROMA_PATH):
+        shutil.rmtree(CHROMA_PATH)
 
-print(chunks[60].page_content)
-print(chunks[60].metadata)
+    db = Chroma.from_documents(
+            documents=chunks, embedding=embedding_function, persist_directory=CHROMA_PATH
+        )
+    db.persist()
+    print(f"Saves {len(chunks)} chunks into a VDB.")
+
+def generate_vdb():
+    documents = load_documents()
+    chunks = split_into_chunks(documents)
+    create_vdb(chunks)
+
+if __name__ == "__main__":
+    generate_vdb()
