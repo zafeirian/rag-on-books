4bec19ef0dd884a54aa0444492b41842063d40e2
zafeirian <zafosnikos@gmail.com>
2025-08-31 17:37:44 +0300

    Created function split_into_chunks()

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 import os
 from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+import re
+from langchain_core.documents import Document
 
 DATA_PATH = "data/books/"
 
-loader = PyPDFLoader(DATA_PATH + "the_hobbit.pdf")
-
-docs = loader.load()
-pages = []
-pages.append({"pages": len(docs)})
-
-for doc in docs:
-    pages.append(doc.page_content)
-
-print(f"Pages: {pages[0]}")
-print(pages[35])
-print(pages[36])
-
 def load_documents():
-    loader = PyPDFLoader(DATA_PATH + "*.pdf")
+    loader = PyPDFLoader(DATA_PATH + "the-hobbit.pdf")
     documents = loader.load()
+    print(len(documents))
+    for doc in documents:
+        doc.page_content = re.sub(r"\s+", " ", doc.page_content).strip()
     return documents
 
+def split_into_chunks(documents: list[Document]):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=500,
+        length_function=len,
+        add_start_index=True
+    )
+    chunks = text_splitter.split_documents(documents)
+    return chunks
+
+documents = load_documents()
+chunks = split_into_chunks(documents)
+
+print(chunks[60].page_content)
+print(chunks[60].metadata)
