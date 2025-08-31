3988f03121e5a73ea578d45f42c7543e31879ef4
zafeirian <zafosnikos@gmail.com>
2025-08-31 16:36:03 +0300

    Created load_documents function

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
 import os
 from dotenv import load_dotenv
+from langchain_community.document_loaders import PyPDFLoader
 
-from langchain_openai import ChatOpenAI
+DATA_PATH = "data/books/"
+
+loader = PyPDFLoader(DATA_PATH + "the_hobbit.pdf")
+
+docs = loader.load()
+pages = []
+pages.append({"pages": len(docs)})
+
+for doc in docs:
+    pages.append(doc.page_content)
+
+print(f"Pages: {pages[0]}")
+print(pages[35])
+print(pages[36])
+
+def load_documents():
+    loader = PyPDFLoader(DATA_PATH + "*.pdf")
+    documents = loader.load()
+    return documents
+
