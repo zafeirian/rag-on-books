2fcea0f6603234d53aa2b40dba00df8d6c23ae43
zafeirian <zafosnikos@gmail.com>
2025-09-01 11:42:29 +0300

    Added LLM on lifespan

--- app.py
+++ app.py
@@ -3,15 +3,24 @@ from pydantic import BaseModel
 from typing import Optional, List
 from dotenv import load_dotenv
 import os
-from langchain_openai import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_chroma import Chroma
-
-
+from langchain.prompts import ChatPromptTemplate
 from contextlib import asynccontextmanager # for lifespan events
 #We need to load the VDB when the application begins and use it for every request (lifespan).
 
 EMBED_MODEL = 'gpt-embedding-3-small'
 CHROMA_PATH = 'chroma'
+LLM_MODEL = 'gpt-4o-mini'
+PROMPT_TEMPLATE = '''
+Answer the question based ONLY on the following context:
+
+{context}
+
+---
+
+Answer the question based on the above context: {query}
+'''
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -30,14 +39,22 @@ async def lifespan(app: FastAPI):
         persist_directory=CHROMA_PATH
     )
 
+    llm = ChatOpenAI(
+        model=LLM_MODEL,
+        api_key=api_key,
+        temperature=0,
+    )
+
     app.state.embedding_function = embedding_function
     app.state.db = db
+    app.state.llm = llm
 
     try:
         yield # Handling requests here.
     finally:
         app.state.db = None
         app.state.embedding_function = None
+        app.state.llm = None
 
 
 class QueryRequest(BaseModel):
@@ -56,12 +73,22 @@ class QueryRespone(BaseModel):
 
 app = FastAPI(title="Simple RAG on LOTR books.", lifespan=lifespan)
 
-@app.get("health")
+@app.get("/health")
 def health():
     return {"ok": True}
 
 @app.post("/query", response_model=QueryRespone)
 def query(req: QueryRequest):
-    db: 
+    db: Chroma = app.state.db
 
-    results = 
+    results = db.similarity_search_with_relevance_scores(query=QueryRequest.text, k=QueryRequest.k)
+    if len(results)==0 or results[0][1]<0.25:
+        return {"Error": "Unable to find relevant content."}
+    
+    context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+    prompt = prompt_template.format(context=context_text, query=QueryRequest.text)
+
+    llm = ChatOpenAI()
+
+    
