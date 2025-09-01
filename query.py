d06695dd1418426b0f3fcae4100d0d3f53d3e225
zafeirian <zafosnikos@gmail.com>
2025-09-01 10:24:02 +0300

    Added LLM's answers, RAG finished

--- query.py
+++ query.py
@@ -3,11 +3,22 @@ from langchain_openai import OpenAIEmbeddings
 from dotenv import load_dotenv
 import os
 import argparse
+from langchain.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
 
 load_dotenv()
 
 CHROMA_PATH = 'chroma'
 
+PROMPT_TEMP = '''
+Answer the question based only on the following context:
+
+{context}
+
+---
+
+Answer the question based on the above context: {query}
+'''
 
 def main():
     parser = argparse.ArgumentParser()
@@ -22,12 +33,20 @@ def main():
     # Searching the VDB
     results = db.similarity_search_with_relevance_scores(query=query_text, k=3)
 
-    if len(results) == 0 or results[0][1]<0.4:
+    if len(results) == 0 or results[0][1]<0.25:
         print(f'Unable to find relevant content.')
         return
 
     context_text = '\n\n---\n\n'.join([doc.page_content for doc, _score in results])
-    print(context_text)
+    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMP)
+    prompt = prompt_template.format(context=context_text, query=query_text)
+
+    llm = ChatOpenAI(model='gpt-4o-mini', api_key=os.getenv('OPENAI_API_KEY'))
+    response_text = llm.invoke(prompt)
+
+    sources = [doc.metadata.get("title", None) for doc, _score in results]
+    formatted_response = f"Response: {response_text.content}\nSources: {sources}"
+    print(formatted_response)
 
 if __name__ == "__main__":
     main()
